29231aa3869f0d89c4905b7030b6a162967540a4
emran07cse <emran07cse@yahoo.com>
2024-09-05 16:52:07 +0600

    add

--- credentials.py
+++ credentials.py
@@ -1,5 +1,5 @@
 # User Credentials
 host = 'localhost'
-user = 'your username here'
-password = 'your password here'
-database = 'student_database'
+user = 'root'
+password = ''
+database = 'hotelmanagementsystem'
